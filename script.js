7af0c302a2075688adb6a65912b9f2775d11a110
Adriana Macedo <adri_mace@hotmail.com>
2025-04-21 22:15:50 +0100

    feat: adicao usuario com token e logout apagando os dados na localstorage

--- script.js
+++ script.js
@@ -43,35 +43,70 @@ const loginForm = document.getElementById("loginForm");
 
 async function login(email, password) {
   try {
-    const response = fetch(`${BACKEND_PATH}/auth/users/login`, {
+    const response = await fetch(`${BACKEND_PATH}/auth/users/login`, {
       method: "POST",
       headers: {
-        "Comtent-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({email, password});
+      body: JSON.stringify({ email, password }),
     });
 
+    if (!response.ok) {
+      alert("Login failed!");
+    } else {
+      const data = await response.json();
 
+      const { user, accessToken } = data;
+
+      // Store user info and the access token in localstorage
+      localStorage.setItem("userName", user.name);
+      localStorage.setItem("userEmail", user.email);
+      localStorage.setItem("accessToken", accessToken);
+      console.log(localStorage);
+      handleSuccessLogin();
+
+      const loginModal = document.getElementById("loginModal");
+      let modal = bootstrap.Modal.getInstance(loginModal);
+      modal.hide();
+    }
   } catch (error) {
     console.log(error);
-    }
+  }
 }
 
-loginForm.addEventListener("submit", async (event)=> {
-event.preventDefault(); 
-const email = document.getElementById("email").value;
-const password = document.getElementById("password").value;
-try {
-  await login(email, password);
-  console.log("logged in sucessfully:");
-} catch (error) {
-  console.log("login failed: ", error);
+function handleSuccessLogin() {
+  const userName = localStorage.getItem("userName");
+  const userEmail = localStorage.getItem("userEmail");
+  const userProfile = document.getElementById("userProfile");
+
+  userProfile.innerHTML = `${userName} => ${userEmail} <a href= "#" id="logout">Logout</a>`;
+  const logincontainer = document.getElementById("loginContainer");
+  logincontainer.style.display = "none";
+
+  const logoutBtn = document.getElementById("logout");
+  logoutBtn.addEventListener("click", handleLogout);
+}
+// criacao de funcao de logout e limpar dados de usuario na localstorage
+function handleLogout() {
+  localStorage.clear();
+  console.log(localStorage);
+  const userProfile = document.getElementById("userProfile");
+  userProfile.innerHTML = ``;
+  const logincontainer = document.getElementById("loginContainer");
+  logincontainer.style.display = "block";
 }
 
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  try {
+    await login(email, password);
+    console.log("logged in sucessfully:");
+  } catch (error) {
+    console.log("login failed: ", error);
+  }
 });
-
-
-
 
 // criar funcao do btn dos filtros
 function handleFilterBtnClick() {
@@ -115,25 +150,45 @@ function handleFilterBtnClick() {
 }
 
 function handleDeleteProduct(event) {
-  if(confirm("tem a ceretza que pretende apagar o seguinte produto")){
-  const productId = event.target.dataset.id;
+  if (confirm("Tem a certeza que pretende apagar o seguinte produto")) {
+    const productId = event.target.dataset.id;
+    const accessToken = localStorage.getItem("accessToken");
+    const headers = new Headers();
+    headers.append("Authorization", `Bearer ${accessToken}`);
 
-  const requestOptions = {
-    method: "DELETE",
-    redirect: "follow",
-  };
+    const requestOptions = {
+      method: "DELETE",
+      redirect: "follow",
+      headers: headers,
+    };
 
-  fetch(`${BACKEND_PATH}/api/products/${productId}`, requestOptions)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error deleting product");
-      }
-      return response.json();
-    })
-    .then(() => {
-      event.target.parentElement.parentElement.parentElement.remove();
-    })
-    .catch((error) => console.error(error));
-}}
+    fetch(`${BACKEND_PATH}/api/products/${productId}`, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          alert("Nao pode apagar este produto!");
+          throw new Error("Error deleting product");
+        }
+        return response.json();
+      })
+      .then(() => {
+        event.target.parentElement.parentElement.parentElement.remove();
+      })
+      .catch((error) => console.error(error));
+  }
+}
 
 handleFilterBtnClick();
+
+function checkLoggedStatus() {
+  const accessToken = localStorage.getItem("accessToken");
+  const userName = localStorage.getItem("userName");
+  const userEmail = localStorage.getItem("userEmail");
+
+  if (accessToken) {
+    const logincontainer = document.getElementById("loginContainer");
+    const userProfile = document.getElementById("userProfile");
+    logincontainer.style.display = "none";
+    userProfile.innerHTML = `${userName}, ${userEmail} <a href="#" id="logout" onclick="handleLogout()">Logout</a>`;
+  }
+}
+checkLoggedStatus();
